2aa2dac27803bdcdf40ef88f34a3f62baed5c428
duyphongdn97 <duyphongdn1997@gmail.com>
2021-06-19 03:34:49 +0700

    update Ex DB

--- Exercise.py
+++ Exercise.py
@@ -1,14 +1,28 @@
 from mysql import connector
+from mysql.connector import errorcode
 
 
 class DBHelper:
     """
-    This is a Database helper class
+    Python Class for connecting  with MySQL server and accelerate development project using MySQL.
     """
+    DB_HOST = 'localhost'
+    DB_PORT = 3306
+    DB_USER = 'abc'
+    DB_PASSWORD = 'abc@#123'
+    DB_NAME = 'test'
 
-    def __init__(self, host: str, port: int, user: str, passwd: str, db: str):
+    connected = False
+    cursor = None
+    connection = None
+    insert_id = ""
+    row_count = 0
+    data = {}
+
+    def __init__(self, host: str = DB_HOST, port: int = DB_PORT, user: str = DB_USER,
+                 passwd: str = DB_PASSWORD, db: str = DB_NAME):
         """
-        This is the constructor creat an object mysql_connector
+        This is the constructor create an object mysql_connector
         :param host: The host name or IP address of the MySQL server.
         :param port: The TCP/IP port of the MySQL server. Must be an integer.
         :param user: The user name used to authenticate with the MySQL server.
@@ -20,136 +34,250 @@ class DBHelper:
         self.__user = user
         self.__passwd = passwd
         self.__db = db
+        if not self.connected:
+            self.connect_mysql()
 
+    def create_table(self, table, params=None):
+        """
+        This method create a new table.
+        :param table: Name of the table
+        :param params: params to execute sql statement
+        """
+        if params is None:
+            params = {}
+
+        # try:
+        query_ = ""
+        for key, value in params.items():
+            query_ += key + " " + value + ', '
+        self.connection = self.connect_mysql()
+        query = "CREATE TABLE " + table + " (" + query_[:-2] + ", primary key (" + key + "))"
+        self.cursor.execute(query)
+        # except Exception:
+        #     print("-----------CREATE TABLE ERROR-----------")
+        #     print("SQL Error: {}".format(self.cursor.statement))
 
     def connect_mysql(self):
         """
-        This method sets up a connection but do not specify the database name
+        This is connect function
+        :except: ER_ACCESS_DENIED_ERROR: raise print("User or password is incorrect!")
+                 ER_BAD_DB_ERROR: raise print("Database not found!")
         """
-        conn = connector.connect(host=self.__host,
-                                 port=self.__port,
-                                 user=self.__user,
-                                 passwd=self.__passwd,
-                                 # db=self.__db, #Do not specify the database name
-                                 charset='utf8')
-        return conn
+        try:
+            # if not self.connected:
+            conn = connector.connect(host=self.__host,
+                                     port=self.__port,
+                                     user=self.__user,
+                                     passwd=self.__passwd,
+                                     db=self.__db,
+                                     charset='utf8')
+            self.cursor = conn.cursor(buffered=True)
+            self.connected = True
+            return conn
+        except connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Access denied! User or password is incorrect! Please check!")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database not found!")
+            else:
+                print(err)
 
-    def connect_database(self):
+    def insert(self, table: str, data=None):
         """
-        This method sets up a connection
+        This method used to insert new records in the table
+        :param table: Name of the table
+        :param data: The records used to insert
         """
-        conn = connector.connect(host=self.__host,
-                                 user=self.__user,
-                                 password=self.__passwd,
-                                 port=self.__port,
-                                 database=self.__db,
-                                 charset='utf8')
-        return conn
+        if data is None:
+            data = {}
+        value_names = []
+        values = []
+        param_storage = []
+        params = []
+        try:
+            if data is dict:
+                for key, value in data.items():
+                    value_names.append(key)
+                    param_storage.append(str(value))
+                    values.append('%s')
+                params = tuple(param_storage)
+                self.connection = self.connect_mysql()
+                query = ("INSERT INTO " + table + "(" + ', '.join(value_names) +
+                         ") ""VALUES(" + ', '.join(values) + ")")
+                self.cursor.execute(query, params)
+            elif isinstance(data, list):
+                for data_ in data:
+                    value_names = []
+                    values = []
+                    param_storage = []
+                    print(data_)
+                    for key, value in data_.items():
+                        value_names.append(key)
+                        param_storage.append(str(value))
+                        values.append('%s')
+                    params_ = tuple(param_storage)
+                    params.append(params_)
+                print(params)
+                self.connection = self.connect_mysql()
+                query = ("INSERT INTO " + table + "(" + ', '.join(value_names) +
+                         ") ""VALUES(" + ', '.join(values) + ")")
+                print(query)
+                self.cursor.executemany(query, params)
+            if hasattr(self.cursor, 'lastrowid'):
+                self.insert_id = self.cursor.lastrowid
+            self.row_count = self.cursor.rowcount
+            self.connection.commit()
 
-    def create_database(self):
-        """
-        This method create database name self.__db
-        """
-        conn = self.connect_mysql()
-        sql = 'create database if not exists ' + self.__db
-        cur = conn.cursor()
-        cur.execute(sql)
-        cur.close()
-        conn.close()
+        except Exception:
+            print("-----------INSERT ERROR-----------")
+            print("SQL Error: {}".format(self.cursor.statement))
 
-    def create_table(self, sql):
+    def select(self, table, where=None, parameters=None, field="*"):
+        if parameters is None:
+            parameters = {}
+        if where is None:
+            where = {}
+        where_or = ''
+        where_and = ''
+        where_params_storage = []
+        where_params = []
+        try:
+            if len(where) > 0:
+                if type(where) is dict:
+                    # SELECT "AND" TYPE ##################################
+                    x = 1
+                    where_items_count = len(where)
+                    for key in where:
+                        if x == where_items_count:
+                            where_and += (key + "='" + where[key] + "'")
+                        else:
+                            where_and += (key + "='" + where[key] + "' AND ")
+                        x += 1
+                    query = "SELECT " + field + " FROM " + table + " WHERE " + where_and + ""
+                elif isinstance(where, list):
+                    # SELECT "OR" TYPE ###################################
+                    x = 1
+                    where_items_count = len(where)
+                    for where_ in where:
+                        for key, value in where_.items():
+                            if x == where_items_count:
+                                where_or += (key + " = %s ")
+                            else:
+                                where_or += (key + " = %s OR ")
+
+                            where_params_storage.append(str(value))
+                            x += 1
+                    where_params = tuple(where_params_storage)
+                    query = "SELECT " + field + " FROM " + table + " WHERE " + where_or + ""
+                else:
+                    # SELECT "OTHER" TYPE ################################
+                    query = "SELECT " + field + " FROM " + table + " WHERE " + where + ""
+
+                self.connection = self.connect_mysql()
+                self.cursor.execute(query, where_params)
+            else:
+                query = "SELECT " + field + " FROM " + table + ""
+                self.connection = self.connect_mysql()
+                self.cursor.execute(query)
+            # return select output
+            return self.cursor.fetchall()
+
+        except Exception:
+            print("-----------SELECT ERROR-----------")
+            print("SQL Error: {}".format(self.cursor.statement))
+
+    def update(self, table: str, parameters=None):
         """
-        This method create a table using sql statement
-        :param sql: sql statement
+        This method used to modify the existing records in the table.
+        :param table: Name of the table
+        :param parameters: new records to modify.
         """
-        conn = self.connect_database()
-        cur = conn.cursor()
-        cur.execute(sql)
-        cur.close()
-        conn.close()
+        if parameters is None:
+            parameters = {}
+        value_name = []
+        params = []
+        try:
+            for key, value in parameters.items():
+                value_name.append(key + " = %s")
+                params.append(value)
 
-    def insert(self, sql, *params):
+            query = "UPDATE " + table + " SET " + ', '.join(value_name) + ""
+            params = tuple(params)
+            self.connection = self.connect_mysql()
+            self.cursor.execute(query, params)
+            if hasattr(self.cursor, 'lastrowid'):
+                self.insert_id = self.cursor.lastrowid
+            self.row_count = self.cursor.rowcount
+            self.connection.commit()
+
+            return self.call_row_count()  # Return 1 or 0. If 1 is returned, the update has been completed; 0 is false.
+
+        except Exception:
+            print("-----------UPDATE ERROR-----------")
+            print("SQL Error: {}".format(self.cursor.statement))
+
+    def delete(self, table, parameters=None):
         """
-        This method insert into table using sql statement
-        :param sql: sql statement
+        This method used to delete existing records in a table
+        :param table: name of the table
+        :param parameters: the records need to deleted
+        :return:
         """
-        conn = self.connect_database()
-        cur = conn.cursor()
-        cur.execute(sql, params)
-        conn.commit()
-        cur.close()
-        conn.close()
+        # global values
+        if parameters is None:
+            parameters = {}
+        params = []
+        value_delete = []
+        # print("DELETE")
+        where = "WHERE " + str(parameters)
+        query = "DELETE FROM " + table + " " + where + ""
+        # print(params)
+        params = tuple(params)
+        self.connection = self.connect_mysql()
+        self.cursor.execute(query, params)
+        self.connection.commit()
+        return self.call_row_count()
 
-    def update(self, sql, *params):
-        """
-        This method update using sql statement
-        :param sql: sql statement
-        :param params: config for sql statement
-        """
-        conn = self.connect_database()
-        cur = conn.cursor()
-        cur.execute(sql, params)
-        conn.commit()
-        cur.close()
-        conn.close()
-
-    def close(self, sql, *param):
-        """
-        This method close connector
-        """
-        conn = self.connect_database()
-        conn.close()
-
-    def delete(self, sql, *params):
-        conn = self.connect_database()
-        cur = conn.cursor()
-        cur.execute(sql, params)
-        conn.commit()
-        cur.close()
-        conn.close()
-
-
-class Person:
-    def __init__(self, name: str, age: int, address: str):
-        self.__name = name
-        self.__age = age
-        self.__address = address
-
-    def get_name(self):
-        return self.__name
-
-    def get_age(self):
-        return self.__age
-
-    def get_address(self):
-        return self.__address
+    def call_row_count(self):
+        return self.row_count
 
 
 if __name__ == '__main__':
-    person0 = Person(name='a', age=1, address='10 abc')
-    person1 = Person(name='b', age=2, address='11 def')
-    person2 = Person(name='c', age=3, address='01 ghi')
+    connect_ = DBHelper()
 
-    # connect SQL
-    test_db_helper = DBHelper('localhost', '3306', 'roots', 'BAPAiIntern2021@', 'Test')
-    test_db_helper.connect_mysql()  # connect SQL
-    test_db_helper.create_database()  # create Database Test
-    test_db_helper.connect_database()  # connect database Test
+    testInsertParams = [{'names': 'ah4324met',
+                         'fak': 'testF123ak',
+                         'Id': '94'
+                         },
+                        {
+                         'names': 'ahmet',
+                         'fak': 'testFak',
+                         'Id': '100'
+                        }]
 
-    # # create Table Person
-    # sql = 'create table Person(name nvarchar(50) primary key, age int, address nvarchar(20))'
-    # test_db_helper.create_table(sql)
+    testUpdateParams = {
+        'names': '565',
+        'fak': 'asd'
+    }
 
-    # # insert data
-    sql = 'insert into Person(name, age, Address) values(%s,%s,%s,%s)'
-    test_db_helper.insert(sql, (person0.get_name(), person0.get_age(), person0.get_address()))
-    #
-    # # update data
-    # sql = 'update Person set age = %s, address = %s where name = %s'
-    # params = (person1.get_age(), person1.get_address(), person1.get_name())
-    # test_db_helper.update(sql, *params)
-    #
-    # # delete data
-    # sql = 'delete from Person where name = %s'
-    # params = ("b")
-    # test_db_helper.delete(sql, *params)
+    testSelectParams = [
+        {
+            'Id': '94'
+        },
+        {
+            'Id': '100'
+
+        }
+    ]
+
+    create_test = {
+        "names": "varchar(50)",
+        "fak": "varchar(50)",
+        "Id": "int"
+    }
+    # connect_.create_table('table1', create_test)
+    # testSelect = connect_.select('table1', testSelectParams)
+    # connect_.insert('table1', testInsertParams)
+    # connect_.update('table1', testUpdateParams)
+    # testDelete2 = connect_.delete('table1', 'Id=100')
+
